23db4ab286fa4958c3536bb4bdf13a8340718343
Lord-Lelsers <millan.kumar.24@bishops.com>
2023-01-19 20:54:35 -0800

    Can move camera with WASD; updating makefile with Vector changes

--- mc_raylib_cpp/main.cpp
+++ mc_raylib_cpp/main.cpp
@@ -9,6 +9,9 @@
 
 #include "include/consts.h"
 
+#include "include/Vector3Util.h"
+// #include "include/Vector2Util.h"
+
 #include "include/cameraController.h"
 
 // using std::vector;
@@ -32,7 +35,30 @@ int main() {
 
     while (!WindowShouldClose()) {
         const float delta = GetFrameTime();
+
         cameraController.update();
+		{
+			Vector3 moveVec = Vector3Zero();
+
+			if (IsKeyDown(KEY_W)) {
+				moveVec += cameraController.calcForward();
+			}
+			if (IsKeyDown(KEY_S)) {
+				moveVec -= cameraController.calcForward();
+			}
+			if (IsKeyDown(KEY_A)) {
+				moveVec -= cameraController.calcRight();
+			}
+			if (IsKeyDown(KEY_D)) {
+				moveVec += cameraController.calcRight();
+			}
+
+			if (moveVec != Vector3Zero()) {
+				moveVec = normalize(moveVec) * delta * 20.0;
+				cameraController.moveBy(moveVec);
+			}
+		}
+		
 
         BeginDrawing();
         {
@@ -44,10 +70,20 @@ int main() {
 			EndMode3D();
         }
         EndDrawing();
+
+
+		std::cout << "delta: " << delta << std::endl;
+
+		float fps = 1.0 / delta;
+		std::cout << "fps: " << fps << std::endl;
+		
+		Vector3 v = cameraController.calcForward();
+		std::cout << v.x << " " << v.y << "" << v.z << std::endl;
     }
 
 
 
 
+	CloseWindow();
 	return 0;
 }
